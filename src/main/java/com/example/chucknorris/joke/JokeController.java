849483f535c9d747dedfc43d74bb8bd2ceda3fdd
Pawel Gorski <pawel.gorski@envelo.pl>
2022-04-08 08:58:18 +0200

    8080 port

--- src/main/java/com/example/chucknorris/joke/JokeController.java
+++ src/main/java/com/example/chucknorris/joke/JokeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 @RequestMapping("/api")
